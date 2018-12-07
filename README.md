b9b7e91e91eb6de8892ea0650fccdbbd46d38f70
xyj1209 <15817061252@163.com>
2018-12-07 10:52:00 +0800

    抽奖v1.0

---
+++ README.md
@@ -0,0 +1,10 @@
+> 本项目是一个抽奖系统
+1. 对服务业务进行拆分，以分布式部署提高连接数
+2. 具体部署部署步骤请看[docker部署抽奖系统](https://www.github.com/xyj1209/docker-compose-lottery-parent)
+
+> 打包前，需要注意的几个地方
+1. 本项目划分为三个模块，lottery-core(抽奖)、lottery-login(登录)、lottery-exposer(暴露接口)
+2. lottery-core需要配置奖池，在application.yml下有奖池的参数配置，目前是划分为特等奖、一等奖、二等奖、三等奖几个奖项，对应po下面的Lottery类，奖项有区别的可以重写这个类。
+
+> 此项目是Maven工程，打包的过程只需要一个命令
+1. 在根目录下打开命令窗口，输入**mvn clean package**
---
+++ sql/lottery.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.6.41 : Database - lottery
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lottery` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `lottery`;
+
+/*Table structure for table `prize` */
+
+DROP TABLE IF EXISTS `prize`;
+
+CREATE TABLE `prize` (
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `prize` */
+
+insert  into `prize`(`start_time`,`end_time`) values ('2018-12-06 09:50:00','2018-12-07 21:50:12');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
